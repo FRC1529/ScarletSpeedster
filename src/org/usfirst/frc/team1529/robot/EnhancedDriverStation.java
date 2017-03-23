c67a0887f3acd81d3976ec26e547abc7b12351de
CyberCards <frc1529@gmail.com>
2017-03-22 20:47:58 -0400

    Some small changes.

--- src/org/usfirst/frc/team1529/robot/EnhancedDriverStation.java
+++ src/org/usfirst/frc/team1529/robot/EnhancedDriverStation.java
@@ -37,7 +37,7 @@ public class EnhancedDriverStation {
 	private Driver parker 	= new Driver(0.2);
 	private Driver athena 	= new Driver(0.2);
 	private Driver tyler	= new Driver(0.05);
-	private Driver currentDriver = parker; // set driver here
+	private Driver currentDriver = tyler; // set driver here
 	
 	/*
 	 * Operator Controls: 8 buttons
